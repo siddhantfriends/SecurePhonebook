ed424fc05cfc99a34abbd0c7b356eabd7b8e0818
w14019334 <w14019334@northumbria.ac.uk>
2017-03-10 18:54:52 +0000

    Michael Chambers Added Search Toolbar - Functions however needs linked into DB Added first Button to the main activity

--- app/src/main/java/uk/ac/northumbria/securephonebook/MainActivity.java
+++ app/src/main/java/uk/ac/northumbria/securephonebook/MainActivity.java
@@ -1,13 +1,35 @@
 package uk.ac.northumbria.securephonebook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Secure Book");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+     searchView = (MaterialSearchView)findViewById(R.id.search_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
     }
 }
