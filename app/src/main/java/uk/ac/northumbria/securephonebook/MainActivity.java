7791f7a41ef0d44011b2767c310fc1a1a41d5bf5
w14019334 <w14019334@northumbria.ac.uk>
2017-03-10 22:01:55 +0000

    Michael Chambers - Linked Register New Contact to AddActivity Began Constructing Database Database Helper created to manage Database

--- app/src/main/java/uk/ac/northumbria/securephonebook/MainActivity.java
+++ app/src/main/java/uk/ac/northumbria/securephonebook/MainActivity.java
@@ -1,35 +1,46 @@
 package uk.ac.northumbria.securephonebook;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity {
     MaterialSearchView searchView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Database database = new Database(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Secure Book");
         toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
 
-     searchView = (MaterialSearchView)findViewById(R.id.search_view);
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+    }
+
+    public void LoadAddActivity (View V) {
+        startActivity(new Intent(this, AddActivity.class));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_item,menu);
+        getMenuInflater().inflate(R.menu.menu_item, menu);
         MenuItem item = menu.findItem(R.id.action_search);
         searchView.setMenuItem(item);
         return true;
     }
+
 }
